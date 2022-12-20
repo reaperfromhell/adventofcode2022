cd5548d3055707b4364e4454101d06f412466b63
reaperfromhell <menino.feio@gmail.com>
2022-12-20 10:41:44 +0000

    day 2 complete

---
+++ src/day2.1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# Python3 program to print 
+# given matrix in spiral form 
+
+total_score = 0
+
+#A = ROCK = 1 = X
+#B = PAPER = 2 = Y
+#C = SCISSORS = 3 = Z 
+
+def calculate_score(line):
+    him, me = line.strip().split(" ")
+    #Him A
+    if him == 'A' and me == 'X':
+        return 3 + 1
+    if him == 'A' and me == 'Y':
+        return 6 + 2
+    if him == 'A' and me == 'Z':
+        return 0 + 3
+    
+    #Him B
+    if him == 'B' and me == 'X':
+        return 0 + 1    
+    if him == 'B' and me == 'Y':
+        return 3 + 2    
+    if him == 'B' and me == 'Z':
+        return 6 + 3
+    
+    #Him C
+    if him == 'C' and me == 'X':
+        return 6 + 1
+    if him == 'C' and me == 'Y':
+        return 0 + 2
+    if him == 'C' and me == 'Z':
+        return 3 + 3
+    
+with open("../input/day2.txt") as file_in:
+    for line in file_in:
+        total_score += calculate_score(line)
+print(total_score)
---
+++ src/day2.2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# Python3 program to solve adventofcode.com/2022  
+
+total_score = 0
+
+#A = ROCK = 1
+#B = PAPER = 2
+#C = SCISSORS = 3
+
+#X = LOSE
+#Y = DRAW
+#Z = WIN
+
+def calculate_score(line):
+    him, me = line.strip().split(" ")
+    #Him A
+    if him == 'A' and me == 'X':
+        return 0 + 3
+    if him == 'A' and me == 'Y':
+        return 3 + 1
+    if him == 'A' and me == 'Z':
+        return 6 + 2
+    
+    #Him B
+    if him == 'B' and me == 'X':
+        return 0 + 1    
+    if him == 'B' and me == 'Y':
+        return 3 + 2    
+    if him == 'B' and me == 'Z':
+        return 6 + 3
+    
+    #Him C
+    if him == 'C' and me == 'X':
+        return 0 + 2
+    if him == 'C' and me == 'Y':
+        return 3 + 3
+    if him == 'C' and me == 'Z':
+        return 6 + 1
+    
+with open("../input/day2.txt") as file_in:
+    for line in file_in:
+        total_score += calculate_score(line)
+print(total_score)
