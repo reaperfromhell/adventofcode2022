e21918ce149d0f6badd21af6a64f9e9dec0de03b
reaperfromhell <menino.feio@gmail.com>
2022-12-20 12:04:34 +0000

    day 4 complete

---
+++ src/day4.1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# Python3 program to solve adventofcode.com/2022 
+overlaps = 0
+
+def calculate_overlaps(line):
+    elf1, elf2 = line.split(",")
+    r1,r2 = elf1.split("-")
+    range1 = range(int(r1),int(r2)+1)
+    r1,r2 = elf2.split("-")
+    range2 = range(int(r1),int(r2)+1)
+
+    if set(range1).issubset(range2) or set(range2).issubset(range1):
+        return 1
+    else:
+        return 0
+    return 0
+
+with open("../input/day4.txt") as file_in:
+    for line in file_in:
+            overlaps += calculate_overlaps(line)
+print(overlaps)
